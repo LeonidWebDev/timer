c25d5b67b3c10549831705d6b2fb5d9a53a3535b
LeonidWebDev <55695468+LeonidWebDev@users.noreply.github.com>
2022-11-08 02:25:41 +0300

    adding html

---
+++ main.js
@@ -0,0 +1,26 @@
+class MyTimer {
+    constructor(hours, minutes, seeconds) {
+        this.hours = document.querySelector(".hours")
+            // this.init()
+    }
+    init() {
+        this.hours.innerHTML = "11122"
+    }
+
+    startTimer() {
+
+    }
+
+    stopTimer() {
+
+    }
+
+    testartTimer() {
+
+    }
+
+
+
+}
+
+let myTimer = new MyTimer()
