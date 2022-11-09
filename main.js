47579512f85f71a1670e47830c0da85cfb684dc2
LeonidWebDev <55695468+LeonidWebDev@users.noreply.github.com>
2022-11-10 01:04:28 +0300

    add stop function and clearTimer

--- main.js
+++ main.js
@@ -2,25 +2,31 @@ class MyTimer {
     constructor() {
         this.startBtn = document.querySelector(".start")
         this.secondsDisplay = document.querySelector(".seconds")
+        this.restartBtn = document.querySelector(".restart")
         this.init()
     }
     init() {
         this.startBtn.addEventListener("click", () => {
             this.startTimer()
         })
+        this.restartBtn.addEventListener("click", () => {
+            this.stopTimer()
+        })
     }
 
     startTimer() {
         let seconds = 0
 
-        window.setInterval(() => {
+        this.intervalID = window.setInterval(() => {
             this.secondsDisplay.innerHTML = seconds
             seconds++
         }, 1000)
+
     }
 
     stopTimer() {
-
+        window.clearInterval(this.intervalID)
+        this.secondsDisplay.innerHTML = 0
     }
 
     testartTimer() {
